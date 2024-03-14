86c5385f8b92085560836fcf2b64ac1ccd306717
pythonpete32 <aaron@DAOresear.ch>
2024-03-14 10:18:39 +0400

    feat: :zap: update userbutton

--- components/UserButton/UserButton.tsx
+++ components/UserButton/UserButton.tsx
@@ -1,28 +1,36 @@
-import { UnstyledButton, Group, Avatar, Text, rem } from '@mantine/core';
+import { UnstyledButton, Group, Avatar, Text, rem, Button } from '@mantine/core';
 import { IconChevronRight } from '@tabler/icons-react';
 import classes from './UserButton.module.css';
 
 export function UserButton() {
+  const isLoggedIn = true;
+
   return (
     <UnstyledButton className={classes.user}>
-      <Group className={classes.userInner}>
-        <Avatar
-          src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-8.png"
-          radius="xl"
-        />
+      {isLoggedIn ? (
+        <Group className={classes.userInner}>
+          <Avatar
+            src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-8.png"
+            radius="xl"
+          />
 
-        <div style={{ flex: 1 }}>
-          <Text size="sm" fw={500}>
-            Harriette Spoonlicker
-          </Text>
+          <div style={{ flex: 1 }}>
+            <Text size="sm" fw={500}>
+              Harriette Spoonlicker
+            </Text>
 
-          <Text c="dimmed" size="xs">
-            hspoonlicker@outlook.com
-          </Text>
-        </div>
+            <Text c="dimmed" size="xs">
+              hspoonlicker@outlook.com
+            </Text>
+          </div>
 
-        <IconChevronRight style={{ width: rem(14), height: rem(14) }} stroke={1.5} />
-      </Group>
+          <IconChevronRight style={{ width: rem(14), height: rem(14) }} stroke={1.5} />
+        </Group>
+      ) : (
+        <Button size="lg" variant="default" fullWidth>
+          Sign up / Log in
+        </Button>
+      )}
     </UnstyledButton>
   );
 }
